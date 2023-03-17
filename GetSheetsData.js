951a166a8274f2a897fef2db0578c35bb4924013
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2023-03-17 20:36:27 -0300

    OKJ

--- GetSheetsData.js
+++ GetSheetsData.js
@@ -1,19 +1,21 @@
-function getsheetdata ()
-{ 
-    const url = "https://sheetdb.io/api/v1/ecg5jzxmp2btz"
-    console.log(url);
-    fetch(url)
-      .then((response) => response.json())
+function getsheetdata() {
+  // const url = "https://sheetdb.io/api/v1/ecg5jzxmp2btz"
+
+
+  const url = "https://opensheet.elk.sh/1_eCRbbUKfJFksUUdJLHTf39I2SRtqrmqOexZZ1rSADs/1";
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json()
       .then((data) => {
-        console.log(data);
-        console.log(data.keys());
+      
+        const found = data.find(element => element.sigla ="BRA");
+        console.log(found.Pontos);
         data.forEach(element => {
           console.log(element.sigla);
           console.log(element.Viés);
-        
-        });
-      
-      });
-};
+       
+        })
 
-  
+      }));
+}
+
