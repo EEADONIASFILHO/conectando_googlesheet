6efb201a1b2d000591f7c8a08f147e3701948f06
Nivaldo Felizola <55410946+Nfelizola@users.noreply.github.com>
2023-03-18 10:02:59 -0300

    ok-final

--- GetSheetsData.js
+++ GetSheetsData.js
@@ -1,21 +1,25 @@
 function getsheetdata() {
-  // const url = "https://sheetdb.io/api/v1/ecg5jzxmp2btz"
-
-
-  const url = "https://opensheet.elk.sh/1_eCRbbUKfJFksUUdJLHTf39I2SRtqrmqOexZZ1rSADs/1";
-  console.log(url);
-  fetch(url)
+// Esta função com ajuda do endpoint da poensheet.elk.sh TEMOS ACESSO A NOSSA SHEET 
+  // para fazermos um fetch e obeter os dados no padroão JSON
+  const site = "https://opensheet.elk.sh/";
+  const sheetid= "1_eCRbbUKfJFksUUdJLHTf39I2SRtqrmqOexZZ1rSADs/1"; // acabando com /1 para acessar 1-planilha
+  var urlacesso = site + sheetid;
+  // quando vc compartilha sua planilha no sheets ela têm um id unico que é a parte essencial do 
+  //link de compartilhamento, é uma espécie de nome codificado de sua planilha aqui estamos chamando de sheetid
+  
+  console.log(urlacesso);
+  fetch(urlacesso)
     .then((response) => response.json()
       .then((data) => {
-      
-        const found = data.find(element => element.sigla ="BRA");
-        console.log(found.Pontos);
+        console.log(data.keys());
+        console.log(data.length + " Países Detectados");
+        // var procurado = data.find(element => element.sigla ="BRA");
+        // console.log(procurado.Pontos);
+        var s="\t";
+        console.log("#"+s+"Sigla"+s+"Pontos"+s+s+"Viés"+s+"Seleção");
         data.forEach(element => {
-          console.log(element.sigla);
-          console.log(element.Viés);
-       
+          console.log(element.ranking+s+element.sigla+"  "+s+element.pontos+s+s+element.viés+s+element.selecao);
         })
 
-      }));
+      }))
 }
-
